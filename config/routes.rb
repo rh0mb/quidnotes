b6fac4233fdcef9de6f85431a36486b40d6c748c
Tomas Alvarez <tomas.alvarez.me@gmail.com>
2013-05-14 20:45:15 -0400

    Created the listings model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Quidnotes::Application.routes.draw do
+  resources :listings
+
+
   devise_for :users
 
   resources :universities
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130514000831) do
+ActiveRecord::Schema.define(:version => 20130515004406) do
+
+  create_table "listings", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "price"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "universities", :force => true do |t|
     t.string   "name"
